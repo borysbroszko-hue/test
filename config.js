d4ab3585e0e2a78771871e2c1f5075069ebcc931
X _ X <borys.broszko@gmail.com>
2026-03-09 19:40:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // config.js
 const appConfig = {
     brandName: "LiquidCloud",
-    categories: ["Wszystkie", "ELFLIQ", "VOZOL SALT", "HQD SALT", "VOZOL SALT PRIME"],
+    // DODANO "Bestsellery"
+    categories: ["Wszystkie", "Bestsellery", "ELFLIQ", "VOZOL SALT", "HQD SALT", "VOZOL SALT PRIME"],
     
     products: [
         {
@@ -10,7 +11,8 @@ const appConfig = {
             category: "VOZOL SALT",
             price: 29.99,
             image: "assets/vozol.png",
-            description: "Soczysta fuzja dojrzałych jagód leśnych i słodkiej brzoskwini. Idealny balans między słodyczą a owocowym orzeźwieniem."
+            description: "Soczysta fuzja dojrzałych jagód leśnych i słodkiej brzoskwini. Idealny balans między słodyczą a owocowym orzeźwieniem.",
+            bestseller: true // <--- TAK OZNACZASZ BESTSELLER
         },
         {
             id: 2,
@@ -18,7 +20,8 @@ const appConfig = {
             category: "ELFLIQ",
             price: 34.50,
             image: "assets/elfliq.png", 
-            description: "Intensywny i głęboki aromat ciemnych winogron z delikatnym, deserowym akcentem. Kultowy smak znany z urządzeń Elf Bar."
+            description: "Intensywny i głęboki aromat ciemnych winogron z delikatnym, deserowym akcentem. Kultowy smak znany z urządzeń Elf Bar.",
+            bestseller: false // lub po prostu nie dodawaj tego pola
         },
         {
             id: 3,
@@ -26,56 +29,9 @@ const appConfig = {
             category: "HQD SALT",
             price: 29.99,
             image: "assets/hqd.png",
-            description: "Cytrusowa eksplozja energii. Połączenie kwaśnej cytryny i aromatycznej limonki, które gwarantuje maksymalne orzeźwienie."
-        },
-        {
-            id: 4,
-            name: "Vozol | Strawberry Ice",
-            category: "VOZOL SALT",
-            price: 29.99,
-            image: "assets/vozol.png",
-            description: "Klasyczna, słodka truskawka przełamana arktycznym chłodem. Wyrazisty smak, który zostaje na długo."
-        },
-        {
-            id: 5,
-            name: "ELFLIQ | Blue Razz Lemonade",
-            category: "ELFLIQ",
-            price: 34.50,
-            image: "assets/elfliq.png", 
-            description: "Orzeźwiająca lemoniada z niebieską maliną. Lekko kwaskowaty, musujący profil smakowy idealny na każdy dzień."
-        },
-        {
-            id: 6,
-            name: "HQD | Watermelon",
-            category: "HQD SALT",
-            price: 29.99,
-            image: "assets/hqd.png",
-            description: "Soczysty, dojrzały arbuz w wydaniu salt. Niesamowicie gładki i naturalnie słodki aromat lata."
-        },
-        {
-            id: 7,
-            name: "Vozol | Mango Apple",
-            category: "VOZOL SALT",
-            price: 29.99,
-            image: "assets/vozol.png",
-            description: "Egzotyczny miks kremowego mango i chrupkiego, zielonego jabłka. Tropikalna przygoda przy każdym zaciągnięciu."
-        },
-        {
-            id: 8,
-            name: "ELFLIQ | Cotton Candy Ice",
-            category: "ELFLIQ",
-            price: 34.50,
-            image: "assets/elfliq.png", 
-            description: "Sentymentalny powrót do smaku waty cukrowej z mocnym, lodowym finiszem. Bardzo słodki i satysfakcjonujący."
-        },
-        {
-            id: 9,
-            name: "HQD | Mint",
-            category: "HQD SALT",
-            price: 29.99,
-            image: "assets/hqd.png",
-            description: "Czysta, lodowata mięta. Klasyczny wybór dla fanów mocnego uderzenia chłodu i totalnego odświeżenia."
-        },
+            description: "Cytrusowa eksplozja energii. Połączenie kwaśnej cytryny i aromatycznej limonki, które gwarantuje maksymalne orzeźwienie.",
+            bestseller: true // <--- TAK OZNACZASZ BESTSELLER
+        }
+        // ... reszta Twoich produktów
     ]
 };
-
