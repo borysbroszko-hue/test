2021b3f9e0767d257fdeda95d6ee085fe0f326ed
rozpierdol <borys.broszko@gmail.com>
2026-03-09 16:54:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,56 +1,80 @@
 // config.js
 const appConfig = {
     brandName: "LiquidCloud",
-    categories: ["Wszystkie", "ELFLIQ", "VOZOL SALT", "HQD SALT", "VOZOL SALT PRIME"],
+    categories: ["Wszystkie", "ELFLIQ", "VOZOL_SALT", "HQD_SALT", "VOZOL_SALT_PRIME"],
     
     products: [
         {
             id: 1,
-            name: "Mango Bongo",
-            category: "Owocowe",
+            name: "Vozol | Berry Peach",
+            category: "VOZOL_SALT",
             price: 29.99,
-            image: "assets/mango.png", // <-- Ścieżka do Twojego lokalnego pliku
-            description: "Soczyste mango prosto z Filipin."
+            image: "assets/vozol.png",
+            description: "Soczysta fuzja dojrzałych jagód leśnych i słodkiej brzoskwini. Idealny balans między słodyczą a owocowym orzeźwieniem."
         },
         {
             id: 2,
-            name: "Vanilla Custard",
-            category: "Deserowe",
+            name: "ELFLIQ | Grape Raisin",
+            category: "ELFLIQ",
             price: 34.50,
-            image: "assets/wanilia.png", // <-- Może być .jpg, .png, .webp itd.
-            description: "Gęsty, waniliowy budyń babci."
-        },
-       {
-            id: 1,
-            name: "Mango Bongo",
-            category: "Owocowe",
-            price: 29.99,
-            image: "assets/mango.png", // <-- Ścieżka do Twojego lokalnego pliku
-            description: "Soczyste mango prosto z Filipin."
+            image: "assets/elfliq.png", 
+            description: "Intensywny i głęboki aromat ciemnych winogron z delikatnym, deserowym akcentem. Kultowy smak znany z urządzeń Elf Bar."
         },
         {
-            id: 2,
-            name: "Vanilla Custard",
-            category: "Deserowe",
-            price: 34.50,
-            image: "assets/wanilia.png", // <-- Może być .jpg, .png, .webp itd.
-            description: "Gęsty, waniliowy budyń babci."
-        },
-       {
-            id: 1,
-            name: "Mango Bongo",
-            category: "Owocowe",
+            id: 3,
+            name: "HQD | Lemon Lime",
+            category: "HQD_SALT",
             price: 29.99,
-            image: "assets/mango.png", // <-- Ścieżka do Twojego lokalnego pliku
-            description: "Soczyste mango prosto z Filipin."
+            image: "assets/hqd.png",
+            description: "Cytrusowa eksplozja energii. Połączenie kwaśnej cytryny i aromatycznej limonki, które gwarantuje maksymalne orzeźwienie."
         },
         {
-            id: 2,
-            name: "Vanilla Custard",
-            category: "Deserowe",
+            id: 4,
+            name: "Vozol | Strawberry Ice",
+            category: "VOZOL_SALT",
+            price: 29.99,
+            image: "assets/vozol.png",
+            description: "Klasyczna, słodka truskawka przełamana arktycznym chłodem. Wyrazisty smak, który zostaje na długo."
+        },
+        {
+            id: 5,
+            name: "ELFLIQ | Blue Razz Lemonade",
+            category: "ELFLIQ",
             price: 34.50,
-            image: "assets/wanilia.png", // <-- Może być .jpg, .png, .webp itd.
-            description: "Gęsty, waniliowy budyń babci."
+            image: "assets/elfliq.png", 
+            description: "Orzeźwiająca lemoniada z niebieską maliną. Lekko kwaskowaty, musujący profil smakowy idealny na każdy dzień."
+        },
+        {
+            id: 6,
+            name: "HQD | Watermelon",
+            category: "HQD_SALT",
+            price: 29.99,
+            image: "assets/hqd.png",
+            description: "Soczysty, dojrzały arbuz w wydaniu salt. Niesamowicie gładki i naturalnie słodki aromat lata."
+        },
+        {
+            id: 7,
+            name: "Vozol | Mango Apple",
+            category: "VOZOL_SALT",
+            price: 29.99,
+            image: "assets/vozol.png",
+            description: "Egzotyczny miks kremowego mango i chrupkiego, zielonego jabłka. Tropikalna przygoda przy każdym zaciągnięciu."
+        },
+        {
+            id: 8,
+            name: "ELFLIQ | Cotton Candy Ice",
+            category: "ELFLIQ",
+            price: 34.50,
+            image: "assets/elfliq.png", 
+            description: "Sentymentalny powrót do smaku waty cukrowej z mocnym, lodowym finiszem. Bardzo słodki i satysfakcjonujący."
+        },
+        {
+            id: 9,
+            name: "HQD | Mint",
+            category: "HQD_SALT",
+            price: 29.99,
+            image: "assets/hqd.png",
+            description: "Czysta, lodowata mięta. Klasyczny wybór dla fanów mocnego uderzenia chłodu i totalnego odświeżenia."
         },
     ]
 };
